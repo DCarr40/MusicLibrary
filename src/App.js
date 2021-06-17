83b70d2e515c6f1cc0844702b62f55826fa4b0bc
DCarr40 <83989305+DCarr40@users.noreply.github.com>
2021-06-16 20:21:52 -0400

    added import for axios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React, {Component} from 'react'
-
+import axios from "axios";
 
 class App extends Component {
-  
-  
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      music: null,
+    };
+  }
 
+  componentDidMount() {
+    console.log("Component mounted!");
+  }
+
+  async fetchMusic() {
+    try {
+      let response = await axios.get("http://www.devcodecampmusiclibrary.com/api/music");
+      this.setState({
+        music: response.data.results,
+      });
+      console.log(this.state.music);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+
+  render() {
+    console.log(this.state.music);
+    console.log("Component rendered!");
     return (
-      <div>
-        
-      </div>
-    )};
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+  
+ 
 }
 
 export default App;
