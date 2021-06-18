3b1b7498fdf083fd5766d364a54d95bb562c7310
DCarr40 <83989305+DCarr40@users.noreply.github.com>
2021-06-17 21:12:21 -0400

    edited MusicTable appearance

--- src/components/MusicTable/MusicTable.jsx
+++ src/components/MusicTable/MusicTable.jsx
@@ -4,8 +4,26 @@ import "./MusicTable.css";
 
 const MusicTable = ({ music }) => {
      let renderedMusic = music.map((song) => {
-        return (
-            <table>
+         return (
+            <div >
+                    <table className ="tbl-content "  cellpadding="0" cellspacing="0" border="0">
+                        <tbody>
+                            <tr>
+                                <td key={song.title}>{song.title}</td>
+                                <td key={song.album}>{song.album}</td>
+                                <td key={song.artist}>{song.artist}</td>
+                                <td key={song.genre}>{song.genre}</td>
+                                <td key={song.releaseDate}>{song.releaseDate}</td>
+                            </tr>
+                        </tbody>
+                    </table>   
+            </div>
+        );
+     });
+
+    return (
+        <div>
+            <table className = "tbl-header " cellpadding="0" cellspacing="0" border="0">
                 <thead>
                     <tr>
                         <th>Title</th>
@@ -15,19 +33,9 @@ const MusicTable = ({ music }) => {
                         <th>Release Date</th>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr>
-                        <td key={song.title}>{song.title}</td>
-                        <td key={song.album}>{song.album}</td>
-                        <td key={song.artist}>{song.artist}</td>
-                        <td key={song.genre}>{song.genre}</td>
-                        <td key={song.releaseDate}>{song.releaseDate}</td>
-                    </tr>
-                </tbody>
             </table>
-        );
-    });
-    return <div>{renderedMusic}</div>;
+            {renderedMusic}
+        </div>);
     };
 
 
