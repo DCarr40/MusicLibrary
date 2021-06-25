468e16c32be7a4473f954fbe6a0af6758d14d91c
DCarr40 <davidwcarruthers@yahoo.com>
2021-06-24 20:17:36 -0400

    added basic design for Form.css

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,23 +1,49 @@
 import React from 'react';
 import "./Form.css";
 
-function TheForm() {
+function TheForm(props){
     return(
-        <div>
-            <body>
-                <form>
-                    <input name="title" type="title"></input>
-                    <input name="album" type="album"></input>
-                    <input name="artist" type="artist"></input>
-                    <input name="genre" type="genre"></input>
-                    <input name="releaseDate" type="releaseDate"></input>
-
-                    <input type="Submit" value="Submit"></input>
-                </form>
-            </body>
+        <div className="form-box">
+            <form>
+                <div className="user-box">
+                    <input></input>
+                    <label>Title</label>
+                </div>
+                <div className="user-box">
+                    <input></input>
+                    <label>Album</label>
+                </div>
+                <div className="user-box">
+                    <input></input>
+                    <label>Artist</label>
+                </div>
+                <div className="user-box">
+                    <input></input>
+                    <label>Genre</label>
+                </div>
+                <div className="user-box">
+                    <input></input>
+                    <label>Release Date</label>
+                </div>
+            </form>
         </div>
     )
- 
 }
-
 export default TheForm;
+//     return (<div>
+
+//     </div>)
+   
+
+
+//     //     <div className="form-box">
+//     //         <form>
+//     //             <div className="user-box">
+//     //                 <input></input>
+//     //                 <label>Username</label>         
+//     //                 <input></input>
+//     //                 <label>Password</label>
+//     //             </div>
+//     //         </form>
+//     //   </div>
+// }
