8c079d9dd1196c688d820cd87993cb0669553896
DCarr40 <davidwcarruthers@yahoo.com>
2021-06-25 08:26:35 -0400

    fixed defaultValue and placeholder in Form

--- src/components/Form/FormComponent.jsx
+++ src/components/Form/FormComponent.jsx
@@ -7,23 +7,23 @@ function Form(props){
         <div className="form-box">
             <form id="myform" >
                 <div className="user-box">
-                    <input type="text" name="title" value=""></input>
+                    <input type="text" name="title" defaultValue="" placeholder="Title"></input>
                     <label>Title</label>
                 </div>
                 <div className="user-box">
-                    <input type="text" name="album" value=""></input>
+                    <input type="text" name="album" defaultValue="" placeholder="Album"></input>
                     <label>Album</label>
                 </div>
                 <div className="user-box">
-                    <input type="text" name="artist" value=""></input>
+                    <input type="text" name="artist" defaultValue="" placeholder="Artist"></input>
                     <label>Artist</label>
                 </div>
                 <div className="user-box">
-                    <input type="text" name="genre" value=""></input>
+                    <input type="text" name="genre" defaultValue="" placeholder="Genre"></input>
                     <label>Genre</label>
                 </div>
                 <div className="user-box">
-                    <input type="text" name="releaseDate" value=""></input>
+                    <input type="text" name="releaseDate" defaultValue="" placeholder="Release Date"></input>
                     <label>Release Date</label>
                 </div>
                 <div className="user-box" >
