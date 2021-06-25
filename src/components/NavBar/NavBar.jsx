580121c4f028b7596f990923514584e4f546f226
DCarr40 <davidwcarruthers@yahoo.com>
2021-06-24 21:12:07 -0400

    edited NavBar.css design

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,11 +7,8 @@ function NavigationBar() {
    
         <nav className="navbar navbar-style fixed-top navbar-expand-lg navbar-light bg-light navbar-style">
             <div className="container-fluid">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                    <a className="navbar-brand active" href="./index.html">Home</a>
+                <div className="extra-padding collapse navbar-collapse center-me" id="navbarTogglerDemo01">
+                    <a className="navbar-brand active center-me" href="./index.html">Music Table</a>
                 </div>
             </div>
         </nav>
