9af8ccd8b955e393935c9a5e7d4143133e8902d9
DCarr40 <davidwcarruthers@yahoo.com>
2021-06-24 22:03:46 -0400

    fixed form desing

--- src/components/Form/FormComponent.jsx
+++ src/components/Form/FormComponent.jsx
@@ -1,29 +1,38 @@
 import React from 'react';
 import "./Form.css";
 
+
 function Form(props){
     return(
         <div className="form-box">
-            <form>
+            <form id="myform">
                 <div className="user-box">
-                    <input></input>
+                    <input type="text" ></input>
                     <label>Title</label>
                 </div>
                 <div className="user-box">
-                    <input></input>
+                    <input type="text" ></input>
                     <label>Album</label>
                 </div>
                 <div className="user-box">
-                    <input></input>
+                    <input type="text" ></input>
                     <label>Artist</label>
                 </div>
                 <div className="user-box">
-                    <input></input>
+                    <input type="text" ></input>
                     <label>Genre</label>
                 </div>
                 <div className="user-box">
-                    <input></input>
+                    <input type="text" ></input>
                     <label>Release Date</label>
+                </div>
+                <div className="user-box">
+                        <button type="submit" >
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            Submit</button>
                 </div>
             </form>
         </div>
